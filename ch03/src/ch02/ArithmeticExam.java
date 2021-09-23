0e3a22fb9672beeeb7a22642649afee53f1e6613
조소현 <@So-426hyun>
2021-09-23 14:20:40 +0900

    ㅇ

--- ch03/src/ch02/ArithmeticExam.java
+++ ch03/src/ch02/ArithmeticExam.java
@@ -17,7 +17,7 @@ public class ArithmeticExam {
 		System.out.println("계산할 초를 정수로 입력하세요:");
 		time = scanner.nextInt();
 		//몫과 나머지
-		int second = time & 60; //5000초 = minute * 60 + 나머지 초
+		int second = time % 60; //5000초 = minute * 60 + 나머지 초
 		int minute = (time/60) % 60;
 		int hour = (time/60)/60;
 		
--- ch03/src/ch02/ArithmeticExam2.java
+++ ch03/src/ch02/ArithmeticExam2.java
@@ -8,8 +8,8 @@ public class ArithmeticExam2 {
 		System.out.println("시분초로 출력하기");
 		
 		//몫과 나머지
-		int second = time & 60; //5000초 = minute * 60 + 나머지 초
-		int minute = (time/60) % 60;System.out.println(time + "초는");
+		int second = time % 60; //5000초 = minute * 60 + 나머지 초
+		int minute = (time/60) % 60;
 		int hour = (time/60)/60;
 		
 		System.out.println(time + "초는");
