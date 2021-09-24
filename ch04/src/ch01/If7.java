2a67d93111aaefeaa9e8df8cefab0f939707b216
조소현 <@So-426hyun>
2021-09-24 14:46:19 +0900

    If

---
+++ ch04/src/ch01/If7.java
@@ -0,0 +1,42 @@
+package ch01;
+
+import java.util.Scanner;
+
+public class If7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("점수를 입력하세요(0~100)");
+		int score = scanner.nextInt();
+		String grade="A";
+		
+		// 점수 >= 90, 점수<95 =>A-, 95==A0, 점수 >95 <=100 => A+
+		if(score>=90) {//grade = "A";를 맨 첫줄에 선언하고 grade+="+" grade+="0" grade+="-"가능
+			if(score<95) grade = "A-";
+			else if(score==95) grade = "A0";
+			else grade ="A+";
+		}
+		else if(score>=80) {//grade = "B";를 맨 첫줄에 선언하고 grade+="+" grade+="0" grade+="-"가능
+			if(score<85) grade = "B-";
+			else if(score==85) grade = "B0";
+			else grade="B+";
+		}
+		else if(score>=70) {
+			if(score<75) grade = "C-";
+			else if(score==75) grade ="C0";
+			else grade ="C+";
+		}
+		else if(score>=60) {
+			if(score<75) grade = "D-";
+			else if(score==75) grade="D0";
+			else grade="D+";
+		}
+		
+		else grade = "F";
+		
+		System.out.println("학점은 " + grade);
+	}
+
+}
---
+++ ch04/src/ch01/RandomExample.java
@@ -0,0 +1,14 @@
+package ch01;
+
+public class RandomExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	  //난수를 이용하여 61~70사이의 무작위의 값 출력
+		//61+0, 61+1, 61+2 ~ 61+9
+		
+		int num = (int)(Math.random()*10); // 0~9
+		System.out.println(61+num);
+	}
+
+}
