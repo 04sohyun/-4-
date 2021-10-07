d43e07a7276d10b95dbd18a9268601d40881c335
조소현 <@So-426hyun>
2021-10-07 16:44:08 +0900

    ㅁ

--- ch06/src/workout/Account1.java
+++ ch06/src/workout/Account1.java
@@ -1,15 +1,15 @@
 package workout;
 
 public class Account1 {
-//static final String bankNo="111";
-//static int seq=0;
+static final String bankNo="111";
+static int seq=0;
  private String ano;
  private String owner;
  private int balance;
- /*{
+ {
 	 seq++;
 	 ano = bankNo +"-"+seq+seq+seq;
- }*/
+ }
  public Account1(String ano, String owner, int balance) {
 	 this.ano=ano;
 	 this.owner=owner;
@@ -23,10 +23,10 @@ public void setOwner(String owner) {this.owner = owner;}
 public int getBalance() {return balance;}
 public void setBalance(int balance) {this.balance = balance;}
 
-//Account 정보 출력 메소드
-//@Override
-/*public String toString() {
+Account 정보 출력 메소드
+@Override
+public String toString() {
 	return "Account [ 계좌번호="+ano+", 계좌주="+ owner+", 잔고="+balance+"]";
-	}*/
+	}
 
  }
